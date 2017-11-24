e3a2fc07910075b9d88b837fc898213f95e64ccb
Baptiste Clavié <clavie.b@gmail.com>
2017-11-25 00:10:43 +0100

    Port env.fish to config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,40 @@ if test -d ~/dotFiles/fish/includes
     end
 end
 
-function src
-    exec fish
+# xterm 256 colors
+set TERM xterm-256color
+
+set VIMRUNTIME $HOME/.vim
+
+# less, utf8 !
+set LESSCHARSET UTF-8
+
+if not contains "$HOME/bin" $PATH
+    set PATH "$HOME/bin" $PATH
 end
+
+# rbenv stuff
+if test -d $HOME/.rbenv
+    if not contains "$HOME/.rbenv/bin" $PATH
+        set PATH "$HOME/.rbenv/bin" $PATH
+    end
+
+    if type -q rbenv
+        rbenv init - fish > /dev/null
+    end
+end
+
+# pip mess
+if not contains "$HOME/.local/bin" $PATH
+    set PATH "$HOME/.local/bin" $PATH
+end
+
+# nvim is da tru editor
+set EDITOR "/usr/bin/nvim"
+
+# fzf
+set FZF_DEFAULT_COMMAND 'ag -l -g "" ./ vendor/'
+set FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+
+# xdg stuff
+set XDG_CONFIG_DIRS "$XDG_CONFIG_DIRS:$HOME/.config/xdg"
