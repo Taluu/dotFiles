f6b94dadbeb229df6c111877802f2ca1ea8c9a36
Baptiste Clavié <clavie.b@gmail.com>
2020-12-30 20:34:28 +0100

    Move from antigen to zplus
    Doesn't seem maintained but let's give it a try, as antigen is
    definitely not maintained anymore...

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -5,14 +5,11 @@ set -k
 HISTSIZE=10000
 SAVEHIST=$HISTSIZE
 
-source $HOME/antigen.zsh
-antigen init $HOME/.antigenrc
+[ -f ~/dotfiles/zsh/zplug.zsh ] && source ~/dotfiles/zsh/zplug.zsh
 
-[ -f "~/dotFiles/env.sh" ] && source "~/dotFiles/env.sh"
+[ -f ~/dotFiles/env.sh ] && source ~/dotFiles/env.sh
 
 export TERM=xterm-256color
-
-autoload -Uz compinit && compinit
 
 #[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
@@ -20,8 +17,8 @@ alias git="LANGUAGE=en_US.UTF-8 \"git\""
 alias vim="LANGUAGE=en_US.UTF-8 \"nvim\""
 alias ls="LANGUAGE=en_US.UTF-8 \"ls\" --color"
 
+# fuck you git plugin, I don't want such aliases
 unalias gm
-# unalias ag
 
 if [[ -x ~/.local/bin/thefuck ]]; then
     alias fuck='eval $(~/.local/bin/thefuck $(fc -ln -1 | tail -n 1)); fc -R'
@@ -40,12 +37,21 @@ export PATH=$PATH:$HOME/bin:$HOME/.config/nvim/pack/packager/opt/phpactor/bin:$H
 
 # nvim is da tru editor
 export EDITOR="/usr/bin/nvim"
+bindkey -v
+
+# let's keep some old "emacs" binding for now, until I get used to vi ones
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+bindkey 'k' history-substring-search-up
+bindkey 'j' history-substring-search-down
+
+bindkey '^A' beginning-of-line
+bindkey '^' beginning-of-line
+bindkey '^E' end-of-line
+bindkey '$' end-of-line
 
 # fzf
 export FZF_DEFAULT_COMMAND='rg -u --files'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 export XDG_CONFIG_DIRS="$XDG_CONFIG_DIRS:$HOME/.config/xdg"
-
-# completion
-complete -C '/usr/bin/aws_completer' aws
