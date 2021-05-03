0344fcc59ac9b02a0af2993fdd745af8b6ca2f49
Baptiste Clavié <clavie.b@gmail.com>
2021-05-03 16:51:07 +0200

    Add the possibility of adding custom zsh config on current machine

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -57,3 +57,5 @@ export FZF_DEFAULT_COMMAND='rg -u --files --follow'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 export XDG_CONFIG_DIRS="$XDG_CONFIG_DIRS:$HOME/.config/xdg"
+
+[ -f ~/.zsh_custom.sh ] && source ~/.zsh_custom.sh
