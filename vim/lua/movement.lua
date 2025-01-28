8fc8d2240694caee94c2e1fcea6241ff4bc9d383
Baptiste Clavié <clavie.b@gmail.com>
2025-01-28 13:37:12 +0100

    Use vim.keymap.set instead of old bloated alias

--- vim/lua/movement.lua
+++ vim/lua/movement.lua
@@ -1,36 +1,35 @@
-local set_key = vim.api.nvim_set_keymap
-local silent_noremap = { silent = true, noremap = true }
-local noremap = { noremap = true }
+local silent_noremap = { silent = true, remap = false }
+local noremap = { remap = false }
 
 -- Moving in files and between tabs & buffers {
     -- between buffers
-    set_key('n', '<M-h>', ':bprev<CR>', silent_noremap)
-    set_key('n', '<M-l>', ':bnext<CR>', silent_noremap)
-    set_key('n', '<M-j>', '<NOP>', silent_noremap)
-    set_key('n', '<M-k>', '<NOP>', silent_noremap)
+    vim.keymap.set('n', '<M-h>', ':bprev<CR>', silent_noremap)
+    vim.keymap.set('n', '<M-l>', ':bnext<CR>', silent_noremap)
+    vim.keymap.set('n', '<M-j>', '<NOP>', silent_noremap)
+    vim.keymap.set('n', '<M-k>', '<NOP>', silent_noremap)
 
     -- between windows
-    set_key('n', '<C-H>', ':wincmd h<CR>', silent_noremap)
-    set_key('n', '<C-J>', ':wincmd j<CR>', silent_noremap)
-    set_key('n', '<C-K>', ':wincmd k<CR>', silent_noremap)
-    set_key('n', '<C-L>', ':wincmd l<CR>', silent_noremap)
+    vim.keymap.set('n', '<C-H>', ':wincmd h<CR>', silent_noremap)
+    vim.keymap.set('n', '<C-J>', ':wincmd j<CR>', silent_noremap)
+    vim.keymap.set('n', '<C-K>', ':wincmd k<CR>', silent_noremap)
+    vim.keymap.set('n', '<C-L>', ':wincmd l<CR>', silent_noremap)
 
     -- between lines
-    set_key('n', 'j', 'gj', noremap)
-    set_key('n', 'k', 'gk', noremap)
+    vim.keymap.set('n', 'j', 'gj', noremap)
+    vim.keymap.set('n', 'k', 'gk', noremap)
 
     -- between tabs
-    set_key('n', '<S-H>', 'gT', noremap)
-    set_key('n', '<S-L>', 'gt', noremap)
-    set_key('n', '<S-J>', '<NOP>', noremap)
-    set_key('n', '<S-K>', '<NOP>', noremap)
+    vim.keymap.set('n', '<S-H>', 'gT', noremap)
+    vim.keymap.set('n', '<S-L>', 'gt', noremap)
+    vim.keymap.set('n', '<S-J>', '<NOP>', noremap)
+    vim.keymap.set('n', '<S-K>', '<NOP>', noremap)
 -- }
 
-set_key('v', '<', '<gv', noremap)
-set_key('v', '>', '>gv', noremap)
+vim.keymap.set('v', '<', '<gv', noremap)
+vim.keymap.set('v', '>', '>gv', noremap)
 
 -- map home / end keys
-set_key('', '[F', '$', {})
-set_key('', '[H', 'g0', {})
+vim.keymap.set('', '[F', '$', {})
+vim.keymap.set('', '[H', 'g0', {})
 
-set_key('', '<Del>', '<NOP>', noremap)
+vim.keymap.set('', '<Del>', '<NOP>', noremap)
