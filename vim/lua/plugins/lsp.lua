42c1ece88511d7a3f45ed0dd4011e9008b092e94
Baptiste Clavié <baptiste.clavie@ouidou.fr>
2026-06-03 16:16:36 +0200

    Use correct form of client:supports_method insted of client...

--- vim/lua/plugins/lsp.lua
+++ vim/lua/plugins/lsp.lua
@@ -47,7 +47,7 @@ return {
 
         -- you can reuse a shared lspconfig on_attach callback here
         on_attach = function(client, bufnr)
-          if client.supports_method("textDocument/formatting") then
+          if client:supports_method("textDocument/formatting") then
             vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
             vim.api.nvim_create_autocmd("BufWritePre", {
               group = augroup,
